b9d0689001aa66ff6c523c000b20219368479dbb
DeinPseudonym <123456+username@users.noreply.github.com>
2025-12-06 04:24:46 +0300

    chore(server): relax eslint rules for any/unused vars

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -11,5 +11,15 @@ module.exports = {
   },
   plugins: ["@typescript-eslint"],
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  rules: {
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_"
+      }
+    ]
+  },
   ignorePatterns: ["dist/**/*", "generated/**/*"]
 };
