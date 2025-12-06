c8c4d7cee1104ca9f7a1108b2529d838794e952e
DeinPseudonym <123456+username@users.noreply.github.com>
2025-12-06 04:29:48 +0300

    chore(server): relax ts-eslint unsafe rules

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -10,7 +10,11 @@ module.exports = {
     sourceType: "module"
   },
   plugins: ["@typescript-eslint"],
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking"
+  ],
   rules: {
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-unused-vars": [
@@ -19,7 +23,17 @@ module.exports = {
         argsIgnorePattern: "^_",
         varsIgnorePattern: "^_"
       }
-    ]
+    ],
+    "@typescript-eslint/no-unsafe-call": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off",
+    "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/no-unsafe-return": "off"
   },
-  ignorePatterns: ["dist/**/*", "generated/**/*"]
+  ignorePatterns: ["dist/**/*", "generated/**/*"],
+  parserOptions: {
+    project: "./tsconfig.json",
+    tsconfigRootDir: __dirname,
+    ecmaVersion: "latest",
+    sourceType: "module"
+  }
 };
